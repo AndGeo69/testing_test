42ff7efd93a04d9ccf745b7b3ad945839b6dbeb6
AndGeo69 <73225938+AndGeo69@users.noreply.github.com>
2021-02-16 15:56:14 +0200

    test_again

---
+++ src/main/java/BigChungus.java
@@ -0,0 +1,15 @@
+public class BigChungus {
+    private String memeStatus = "";
+
+    public String getMemeStatus() {
+        return memeStatus;
+    }
+
+    public void setMemeStatus(String memeStatus) {
+        this.memeStatus = memeStatus;
+    }
+
+    public BigChungus(String memeStatus) {
+        this.memeStatus = memeStatus;
+    }
+}
